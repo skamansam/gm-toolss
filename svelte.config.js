58bbfe3a3abdf3976bf2f0cad8ba8101a0d8e38f
Samuel C Tyler <skamansam@gmail.com>
2025-11-01 15:54:09 -0400

    better config for sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
@@ -18,7 +14,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: dev ? '' : process.env.BASE_PATH || "gm-tools"
+			base: process.env.NODE_ENV === 'production' ? '/gm-tools' : ''
 		}
 	}
 };
